4e622629874b96198b78f98355b6a4609088e59a
Chloe Mintel <mintelc12@gmail.com>
2022-06-17 09:20:51 -0400

    Finished with the help of some friends.

--- word_frequency.py
+++ word_frequency.py
@@ -6,59 +6,53 @@ STOP_WORDS = [
 
 import string
 
-#def print_word_freq(file):
-"""Read in `file` and print out the frequency of words in that file. Your code goes here."""
-    #print (f"Your file is: {file}")
-    #with open(file) as open_file:
-        #read_file = open_file.read()
-    #print(read_file)
-test_sentence = "How long, how long must we sing this song? How long?"
+def print_word_freq(file):
+    """Read in `file` and print out the frequency of words in that file. Your code goes here."""
+    print (f"Your file is: {file}")
+    with open(file) as open_file:
+        read_file = open_file.read()
+    # print(read_file)
 
-new_string = test_sentence.translate(str.maketrans("", "", string.punctuation))
-print(test_sentence.lower())
+# for word in 
 
+    poem = read_file.translate(str.maketrans("", "", string.punctuation))
+    poem = poem.lower()
+    words = poem.split()
+    # print(poem)
+    # print(words)
+    for word in words.copy():
+        if word in STOP_WORDS:
+            words.remove(word)
+    unique_words = {"song": 1}
 
-new_string = new_string.lower()
-words = new_string.split()
-unique = {"song": 1}
-# value = 1
-for word in words:
-    if word not in unique:
-        unique.update({word: 1}) 
-    else:
-        unique[word]
-        #{word: (value + 1)}
-        print(unique[word])
-
-
-    #new_string = new_string.split()
-    #for word in new_string:
-        #if word in STOP_WORDS:
-            #new_string = list(filter((word).__ne__, new_string))
-    #print(new_string)
-
-#print_word_freq("praise_song_for_the_day.txt")
-
-
-
+    for word in words:
+        if word not in unique_words:
+            unique_words[word] = 1 
+        else:
+            unique_words[word] +=1
+            #{word: (value + 1)}
+            # print(unique_words[word])
+    sort_unique_words = sorted(unique_words.items(), key=lambda x: x[1], reverse=True)
+    for i in sort_unique_words[:10]:
+        print(f'{i[0]:10} | {i[1]} {"*" * i[1]}')
+    # print(unique_words)
 
 
 
 
 #python program that allows you to run from the command line and pass the name of the file
+if __name__ == "__main__":
+    import argparse
+    from pathlib import Path
 
-# if __name__ == "__main__":
-#     import argparse
-#     from pathlib import Path
+    parser = argparse.ArgumentParser(
+        description='Get the word frequency in a text file.')
+    parser.add_argument('file', help='file to read')
+    args = parser.parse_args()
 
-#     parser = argparse.ArgumentParser(
-#         description='Get the word frequency in a text file.')
-#     parser.add_argument('file', help='file to read')
-#     args = parser.parse_args()
-
-#     file = Path(args.file)
-#     if file.is_file():
-#         print_word_freq(file)
-#     else:
-#         print(f"{file} does not exist!")
-#         exit(1)
+    file = Path(args.file)
+    if file.is_file():
+        print_word_freq(file)
+    else:
+        print(f"{file} does not exist!")
+        exit(1)
